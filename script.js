0035065cd5e8b4f2971a61282e355ee1892816b0
amiksayed <amiksayed34@gmail.com>
2022-05-29 23:32:06 +0600

    re-edited

--- script.js
+++ script.js
@@ -11,14 +11,18 @@ const savingAmountDisplay = document.getElementById('saving-amount');
 const remainingBalanceDisplay = document.getElementById('remaining-balance-display');
 const remainingBalanceErrorMessage = document.querySelector('.remaining-balance-error-message');
 const remainingBalanceDiv = document.getElementById('remaining-balance');
+const inputErrorMessage = document.querySelector('.income-error-message', 'food-error-message', '.rent-error-message', '.clothes-error-message');
 
 function negativeInput(input, error) {
-    const incomeErrorMessage = document.querySelector(error);
+    const inputErrorMessage = document.querySelector(error);
     if (input < 0) {
-        incomeErrorMessage.innerText = 'Enter a valid value!';
+        inputErrorMessage.innerText = 'Enter a valid value!';
+        inputErrorMessage.style.display = 'block';
+    }
+    else {
+        inputErrorMessage.style.display = 'none';
     }
 }
-
 
 document.getElementById('calculate-button').addEventListener('click', function () {
     negativeInput(incomeInput.value, '.income-error-message');
@@ -27,11 +31,12 @@ document.getElementById('calculate-button').addEventListener('click', function (
     negativeInput(clothInput.value, '.clothes-error-message');
     if (incomeInput.value > 0 && foodInput.value > 0 && rentInput.value > 0 && clothInput.value > 0) {
         const totalExpense = parseInt(foodInput.value) + parseInt(rentInput.value) + parseInt(clothInput.value);
-        if (totalExpense < incomeInput.value) {
-
+        if (totalExpense <= incomeInput.value) {
             const totalBalance = parseInt(incomeInput.value) - totalExpense;
             totalExpenseDisplay.innerText = totalExpense;
             balanceDisplay.innerText = totalBalance;
+            balanceErrorMessage.style.display = 'none';
+            balanceDiv.style.display = 'block';
         }
 
         else {
@@ -40,16 +45,36 @@ document.getElementById('calculate-button').addEventListener('click', function (
             balanceDiv.style.display = 'none';
         }
     }
+    foodInput.value = '';
+    rentInput.value = '';
+    clothInput.value = '';
+    saveInput.value = '';
 })
 
 document.getElementById('save-button').addEventListener('click', function () {
     savingAmountDisplay.innerText = incomeInput.value * (saveInput.value / 100);
     if (balanceDisplay.innerText >= savingAmountDisplay.innerText) {
-        console.log(balanceDisplay.innerText );
         remainingBalanceDisplay.innerText = balanceDisplay.innerText - savingAmountDisplay.innerText;
+        // remainingBalanceErrorMessage.style.display = 'none';
+        remainingBalanceDiv.style.display = 'block';
     }
     else {
         remainingBalanceErrorMessage.innerText = 'You dont have sufficient balance';
         remainingBalanceDiv.style.display = 'none';
+        remainingBalanceErrorMessage.style.display = 'block';
     }
+    incomeInput.value = '';
+    saveInput.value = '';
+})
+
+document.getElementById('calculate-button').addEventListener('blur', function () {
+    negativeInput(incomeInput.value, '.income-error-message');
+    negativeInput(foodInput.value, '.food-error-message');
+    negativeInput(rentInput.value, '.rent-error-message');
+    negativeInput(clothInput.value, '.clothes-error-message');
+    inputErrorMessage.style.display = 'none';
+})
+
+document.getElementById('save-button').addEventListener('blur', function () {
+    remainingBalanceErrorMessage.style.display = 'none';
 })
